807b0bb7a76d0116ace9e0453c7af9a7c209e655
Jakub Pastucha <kuba.pastucha@gmail.com>
2023-11-03 11:01:54 +0100

    change double quotes to single quotes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import PianoRoll from "./pianoroll.js";
+import PianoRoll from './pianoroll.js';
 
 class PianoRollDisplay {
     constructor(csvURL) {
@@ -8,55 +8,55 @@ class PianoRollDisplay {
 
     async loadPianoRollData() {
         try {
-            const response = await fetch("https://pianoroll.ai/random_notes");
+            const response = await fetch('https://pianoroll.ai/random_notes');
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             this.data = await response.json();
         } catch (error) {
-            console.error("Error loading data:", error);
+            console.error('Error loading data:', error);
         }
     }
 
     preparePianoRollCard(rollId) {
-        const cardDiv = document.createElement("div");
-        cardDiv.classList.add("piano-roll-card");
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('piano-roll-card');
 
         // Create and append other elements to the card container as needed
-        const descriptionDiv = document.createElement("div");
-        descriptionDiv.classList.add("description");
+        const descriptionDiv = document.createElement('div');
+        descriptionDiv.classList.add('description');
         descriptionDiv.textContent = `This is a piano roll number ${rollId}`;
         cardDiv.appendChild(descriptionDiv);
 
         const svg = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "svg"
+            'http://www.w3.org/2000/svg',
+            'svg'
         );
-        svg.classList.add("piano-roll-svg");
-        svg.setAttribute("width", "80%");
-        svg.setAttribute("height", "150");
+        svg.classList.add('piano-roll-svg');
+        svg.setAttribute('width', '80%');
+        svg.setAttribute('height', '150');
 
         // Append the SVG to the card container
         cardDiv.appendChild(svg);
 
         // **************************************************************************
         // Make each Piano Roll clickable
-        cardDiv.addEventListener("click", function () {
+        cardDiv.addEventListener('click', function () {
             const activeCards = document.querySelectorAll(
-                ".piano-roll-card.active"
+                '.piano-roll-card.active'
             );
 
             // remove useless class from every cardDiv
             activeCards.forEach((activeCard) => {
-                activeCard.classList.remove("active");
-                const svg = activeCard.querySelector(".piano-roll-svg");
-                svg.setAttribute("height", "150");
+                activeCard.classList.remove('active');
+                const svg = activeCard.querySelector('.piano-roll-svg');
+                svg.setAttribute('height', '150');
             });
 
             // add class "active" to clicked element (item becomes the main element on the page)
-            cardDiv.classList.add("active");
-            const svg = cardDiv.querySelector(".piano-roll-svg");
-            svg.setAttribute("height", "250");
+            cardDiv.classList.add('active');
+            const svg = cardDiv.querySelector('.piano-roll-svg');
+            svg.setAttribute('height', '250');
         });
         // **************************************************************************
 
@@ -68,8 +68,8 @@ class PianoRollDisplay {
         if (!this.data) return;
 
         const pianoRollContainer =
-            document.getElementById("pianoRollContainer");
-        pianoRollContainer.innerHTML = "";
+            document.getElementById('pianoRollContainer');
+        pianoRollContainer.innerHTML = '';
         for (let it = 0; it < 20; it++) {
             const start = it * 60;
             const end = start + 60;
@@ -83,7 +83,7 @@ class PianoRollDisplay {
     }
 }
 
-document.getElementById("loadCSV").addEventListener("click", async () => {
+document.getElementById('loadCSV').addEventListener('click', async () => {
     const csvToSVG = new PianoRollDisplay();
     await csvToSVG.generateSVGs();
 });
